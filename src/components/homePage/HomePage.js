ebbf7fdbac017e51dd3bc0d425140be6406e0222
Gialdini Javier <jngialdini19@gmail.com>
2023-05-02 20:45:27 -0300

    navbar y homepage

---
+++ src/components/homePage/HomePage.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './HomePage.css'
+
+export const HomePage = () => {
+    return(
+        <div className="homePage">
+            <div>
+                <p className="textTitulo">Nomades Digitales</p>
+                <p className="textSubtitulo">En cualquier lugar, en cualquier momento</p>
+            </div>
+        </div>
+    )
+}
